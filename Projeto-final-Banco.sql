d18792ef327b43ea96a325fdd78949faa8de279a
anacamorims <ana.camorims.dev@gmail.com>
2024-06-10 21:40:00 -0400

    added tables

--- Projeto-final-Banco.sql
+++ Projeto-final-Banco.sql
@@ -15,3 +15,23 @@ CREATE TABLE `cidade` (
   FOREIGN KEY (`id_estado`) REFERENCES `estado`(`id`)
 );
 
+CREATE TABLE `pessoa` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(100),
+  `sobrenome` varchar(100),
+  `data_de_nascimento` date,
+  `genero` char(1),
+  `tel_contato` varchar(50),
+  `email` varchar(50)
+);
+
+CREATE TABLE `documento` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(50),
+  `sigla` varchar(10),
+  `numero` int,
+  `id_uf` int,
+  `data_emissao` date,
+  FOREIGN KEY (`id_uf`) REFERENCES `estado`(`id`)
+);
+
