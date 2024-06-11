829fccdb4a2bfa11cfa2562da3371147fd111444
anacamorims <ana.camorims.dev@gmail.com>
2024-06-11 19:47:34 -0400

    add file analise

---
+++ db-triagem-hospitalar-analise.sql
@@ -0,0 +1,15 @@
+
+USE triagem_hospitalar;
+
+-- Selecionando os estados e siglas
+SELECT nome, sigla FROM estado;
+
+
+
+-- Selecionando pessoas que moram numa cidade específica
+SELECT pessoa.nome, pessoa.sobrenome 
+FROM pessoa 
+INNER JOIN pessoa_endereco ON pessoa.id = pessoa_endereco.id_pessoa 
+INNER JOIN cidade ON pessoa_endereco.id_cidade = cidade.id 
+WHERE cidade.nome = 'São Paulo';
+
