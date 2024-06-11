b69c902fcff662170e52df92f48ab0cf0ef09e09
Lauanny <ramalholauanny5@gmail.com>
2024-06-11 20:52:12 -0300

    add consultas

--- db-triagem-hospitalar-analise.sql
+++ db-triagem-hospitalar-analise.sql
@@ -13,3 +13,26 @@ INNER JOIN pessoa_endereco ON pessoa.id = pessoa_endereco.id_pessoa
 INNER JOIN cidade ON pessoa_endereco.id_cidade = cidade.id 
 WHERE cidade.nome = 'São Paulo';
 
+
+
+USE triagem_hospitalar;
+
+-- Selecionando os estados e siglas
+SELECT nome, sigla FROM estado;
+
+
+
+-- Selecionando pessoas que moram numa cidade específica
+SELECT pessoa.nome, pessoa.sobrenome 
+FROM pessoa 
+INNER JOIN pessoa_endereco ON pessoa.id = pessoa_endereco.id_pessoa 
+INNER JOIN cidade ON pessoa_endereco.id_cidade = cidade.id 
+WHERE cidade.nome = 'São Paulo';
+
+
+
+-- Consultando profissionais e suas especialidades
+SELECT pessoa.nome, pessoa.sobrenome, profissional_especialidade.nome AS especialidade 
+FROM profissional 
+INNER JOIN pessoa ON profissional.id_pessoa = pessoa.id 
+INNER JOIN profissional_especialidade ON profissional.id_profissional_especialidade = profissional_especialidade.id;
